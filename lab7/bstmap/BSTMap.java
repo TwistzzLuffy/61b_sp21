1b8dd2dee462e44617020ba2f777a9ef91a25fb5
TwistzzLuffy <1060032653@qq.com>
2022-04-10 15:36:28 +0800

    lab7

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,129 @@
+package bstmap;
+
+import com.sun.jdi.Value;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap <K extends Comparable<K>,V> implements Map61B<K,V> {
+    private Node root;
+    private class Node{
+        private K key;
+        private V val;
+        private Node left,right;
+        private int size;
+        public Node(K key,V val,int size){
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+    public BSTMap(){
+    }
+
+    /* Removes all of the mappings from this map. */
+    public void clear(){
+        root = null;
+    }
+
+    @Override
+    /* Returns true if this map contains a mapping for the specified key. */
+    public boolean containsKey(K key){
+        if (key == null ){
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return containsKey(root,key);
+    }
+    private boolean containsKey(Node x,K key){
+        int cmp = key.compareTo(x.key);
+        if (cmp > 0){
+            return containsKey(x.right,key);
+        }else if(cmp < 0){
+            return containsKey(x.right,key);
+        }
+        return true;
+    }
+
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    public V get(K key){
+        return get(root,key);
+    }
+
+    private V get(Node x,K key){
+        if (key == null){
+            throw new IllegalArgumentException("calls get() with a null key ");
+        }
+        if(x == null ){
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0){
+            return x.val;
+        }else if (cmp < 0){
+            return get(x.left,key);
+        }else {
+            return get(x.right,key);
+        }
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    public int size(){
+        return size(root);
+    }
+
+    private int size(Node x){
+        if(x == null){
+            return 0;
+        }
+        return x.size;
+    }
+
+    /* Associates the specified value with the specified key in this map. */
+    public void put(K key, V val){
+        root = put(root,key,val);
+    }
+
+    private Node put(Node x,K key,V val){
+        if (x == null){
+            return new Node(key,val,1);
+        }
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0){
+            x.left = put(x.left,key,val);
+        }else if(cmp > 0){
+            x.right = put(x.right,key,val);
+        }else{
+            x.val = val;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+
+    /* Returns a Set view of the keys contained in this map. Not required for Lab 7.
+     * If you don't implement this, throw an UnsupportedOperationException. */
+   public Set<K> keySet(){
+       throw new UnsupportedOperationException("this method isn't provided");
+    }
+
+    /* Removes the mapping for the specified key from this map if present.
+     * Not required for Lab 7. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    public V remove(K key){
+        throw new UnsupportedOperationException("this method isn't provided");
+    }
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for Lab 7. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    public V remove(K key, V value){
+        throw new UnsupportedOperationException("this method isn't provided");
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+}
